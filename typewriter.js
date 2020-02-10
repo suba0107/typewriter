5c61ea434e8a3bb30cbb610d3e76a114d770f2dc
Subangi <suba0107@stud.kea.dk>
2020-02-10 20:34:27 +0100

    Kan vise et bogstav ad gangen

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,35 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", show);
+
+// declare all global variables
+const fullText = document.querySelector(".typewritten").textContent.trim();
+let write;
+let add;
+let writing = -1;
+let text = "";
+
+// function start() {
+//   document.querySelector(".typewritten").textContent = text;
+//   tjek();
+// }
+function show() {
+  // Vis én karakter
+  add = `${fullText.charAt(writing)}`;
+  text = `${text}${add}`;
+  document.querySelector(".typewritten").textContent = text;
+  tjek();
+}
+
+function tjek() {
+  // Tjek om alle karakterne er skrevet ellers gå til næste bogstav
+  if (writing < fullText.length) {
+    addChart();
+  }
+}
+
+function addChart() {
+  //Add 1 to writing
+  writing++;
+  setTimeout(show, 200);
+}
