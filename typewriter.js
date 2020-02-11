be41a61046b15b91c16ebe3e916f081a24e4b8f1
Subangi <suba0107@stud.kea.dk>
2020-02-11 11:46:51 +0100

    Nu med loop...

--- typewriter.js
+++ typewriter.js
@@ -1,18 +1,20 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", show);
+document.addEventListener("DOMContentLoaded", start);
 
 // declare all global variables
 const fullText = document.querySelector(".typewritten").textContent.trim();
 let write;
 let add;
-let writing = -1;
-let text = "";
+let writing;
+let text;
 
-// function start() {
-//   document.querySelector(".typewritten").textContent = text;
-//   tjek();
-// }
+function start() {
+  writing = -1;
+  text = "";
+  show();
+}
+
 function show() {
   // Vis én karakter
   add = `${fullText.charAt(writing)}`;
@@ -25,11 +27,13 @@ function tjek() {
   // Tjek om alle karakterne er skrevet ellers gå til næste bogstav
   if (writing < fullText.length) {
     addChart();
+  } else {
+    start();
   }
 }
 
 function addChart() {
-  //Add 1 to writing
+  //Add 1 to writing og sæt tid
   writing++;
   setTimeout(show, 200);
 }
